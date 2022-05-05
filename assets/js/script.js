c7fb486dc2648b21e57b80ef3874896b679b212e
hoganrsherrow <hogan.r.sherrow@vanderbilt.edu>
2022-05-04 23:13:45 -0500

    added arrays with different types of characters and fixed html link

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,14 @@
-// Assignment code here
+// Arrays that hold different characters
+var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'l', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var numerals = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+var specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', '-'];
+
+
+// User interaction questions
+
+
+// Password generator
+
 
 
 // Get references to the #generate element
